cc6695ae2d52b88548e7a0e78ee31885b2702754
ANKAY <52236029+ankay212000@users.noreply.github.com>
2019-12-30 10:38:28 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/ankay/Android/Sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/ankay/AndroidStudioProjects/flutter_app/test_version"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/ankay/Android/Sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
